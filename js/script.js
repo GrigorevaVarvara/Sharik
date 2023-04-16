29121be7db31b2e3014df1c2e01cd5255f3274b3
Varvara <varvara.grgio@yandex.ru>
2023-04-16 16:50:32 +0300

    13133

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/*бургер меню */
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+function toggleMenu(){
+    burger.classList.toggle('open');
+    menu.classList.toggle('open');
+}
+
 var links = document.querySelectorAll(".itemLinks");
 var wrapper = document.querySelector("#wrapper");
 
